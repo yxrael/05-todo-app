9c8dff6cc3ab2352bcc19b31ee47279c9ef9dbf5
Israel Moreno <yxrael@gmail.com>
2024-05-21 08:25:01 +0200

    first commit

---
+++ src/todos/models/todo.model.js
@@ -0,0 +1,18 @@
+
+
+export class Todo {
+
+    /**
+     * 
+     * @param {String} description 
+     */
+
+    constructor( description ){
+        this.id = 1;
+        this.description = description;
+        this.done = false;
+        this.createdAt = new Date();
+    }
+
+
+}
