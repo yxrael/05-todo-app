a2b8de38cdc04c05317104fc5c14bbb281ddc672
Israel Moreno <yxrael@gmail.com>
2024-05-21 22:29:09 +0200

    store creada

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -11,6 +11,8 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra de la realidad'),
     ],
     filter: Filters.All,
 }
@@ -21,6 +23,79 @@ const initStore = () => {
     console.log('InitStore');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = ( filter = Filters.All) => {
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.filter( todo => todo.done );
+            case Filters.Pending:
+                return state.filter( todo => !todo.done );
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+            break;
+    }
+}
+
+/**
+ * 
+ * @param {String} todo 
+ */
+const addTodo = ( description ) => {
+    if ( !description ) throw new Error('Description is required');
+
+    state.todos.push( new Todo( description ));
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+    state.todos = state.todos.map( todo => {
+        if ( todo.id === todoId ) {
+            todo.done = !todo.done
+        }
+        return todo;
+    })
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.is !== todoId );
+}
+
+const deleteCompleted  = () => {
+    state.todos = state.todos.filter( todo => !todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
     initStore,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
+    toggleTodo,
+    loadStore,
+    addTodo,
+    getTodos
 }
